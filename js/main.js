bcffd44ce89d328f24f7f444a627d5d9247304ab
EduardoOlay <lalo.olay@hotmial.com>
2021-01-11 02:17:57 -0600

    Agregue que en vez de texto sea imagenes y que muestre el nombre y no de hexagrama que es

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-var tabla,lista,celda,texto;
-var lhexa=new Array(5);
+var tabla,lista,celda;
+const lhexa=[];
+
 
 function iniciar(){
  tabla=document.getElementById("hexg");
@@ -22,8 +23,7 @@ function iniciar(){
 
   //Tabla 1
   celda=document.createElement("tr");
-  texto=document.createTextNode(valor);
-  celda.appendChild(texto);
+  celda.appendChild(valor);
   celda.classList.add("linea");
   if (lista.hasChildNodes()){
      lista.insertBefore(celda,lista.childNodes[0]); 
@@ -35,8 +35,7 @@ function iniciar(){
 
   //Tabla 2
   celda2=document.createElement("tr");
-  texto2=document.createTextNode(valor2);
-  celda2.appendChild(texto2);
+  celda2.appendChild(valor2);
   if (lista2.hasChildNodes()){
      lista2.insertBefore(celda2,lista2.childNodes[0]); 
   } 
@@ -47,8 +46,7 @@ function iniciar(){
 
   //Tabla 3
   celda3=document.createElement("tr");
-  texto3=document.createTextNode(valor3);
-  celda3.appendChild(texto3);
+  celda3.appendChild(valor3);
   if (lista3.hasChildNodes()){
      lista3.insertBefore(celda3,lista3.childNodes[0]); 
   } 
@@ -64,6 +62,8 @@ function borrarHexagrama(){
     lista.removeChild(lista.lastChild);
     lista2.removeChild(lista2.lastChild);
     lista3.removeChild(lista3.lastChild);
+    lhexa.pop();
+    console.log(lhexa);
   }
 }
 
@@ -73,6 +73,8 @@ function borrarLinea(){
   	 lista.removeChild(lista.childNodes[0]);
      lista2.removeChild(lista2.childNodes[0]);
      lista3.removeChild(lista3.childNodes[0]);
+     lhexa.pop();
+     console.log(lhexa);
 }
 
 
@@ -123,63 +125,78 @@ function verificaVal(){//funcion que verifica los valores
 }
 
 function ConvLineas(x){//funcion que retornara la linea correspondiente dependiendo el resultado de la suma
-    var seis ="____________x_____________";
-    var siete="__________________________";
-    var ocho ="____________ _____________";
-    var nueve="____________O_____________";
+    var yinM=new Image();
+    yinM.src="images/Yin mutante.png";
+
+    var yang=new Image();
+    yang.src="images/Yang.png";
+
+    var yin=new Image();
+    yin.src="images/Yin.png";
+
+    var yanM=new Image();
+    yanM.src="images/Yang mutante.png";
 
     if(x==6){
-        return seis;
+        return yinM;
     }
 
     if(x==7){
-        return siete;
+        return yang;
     }
 
     if(x==8){
-        return ocho;
+        return yin;
     }
 
     if(x==9){
-        return nueve;
+        return yanM;
     }
 }
 
 function ConvLineas2(x){ //Tabla 2
-    var siete="__________________________";
-    var ocho ="____________ _____________";
+    var yang=new Image();
+    yang.src="images/Yang.png";
+
+    var yin=new Image();
+    yin.src="images/Yin.png";
+
     if(x == 6){
-        return ocho;
+        return yin;
     }
     if(x == 9){
-        return siete;
+        return yang;
     }
     if(x == 7){
-        return siete;
+        return yin;
     }
     if(x == 8){
-        return ocho;
+        return yin;
     }
 }
 
 function ConvLineas3(x){ // Tabla 3
-    var siete="__________________________";
-    var ocho ="____________ _____________";
+    var yang=new Image();
+    yang.src="images/Yang.png";
+
+    var yin=new Image();
+    yin.src="images/Yin.png";
+
     if(x == 6){
-        lhexa.push(siete);
-        return siete;
+        lhexa.push(1);
+        return yang;
     }
     if(x == 9){
-        lhexa.push(ocho);
-        return ocho;
+        lhexa.push(2);
+        return yin;
     }
     if(x == 7){
-        lhexa.push(siete);
-        return siete;
+        lhexa.push(1);
+        return yang;
     }
     if(x == 8){
-        lhexa.push(ocho);
-        return ocho;
+        lhexa.push(2);
+        return yin;
     }
 }
 
@@ -196,7 +213,60 @@ let verificarNumero = (text) => {
 
 }
 
+
+//Aqui se van comparando el arreglo guardado con cada una de las posblies opciones de hexagrama
 function mostrarNomyNumHexa(){
 
-  console.log(lhexa);
-}
+  //1 es yang ____________
+  //2 es yin  ____   _____
+
+    const hexa1 = [1,1,1,1,1,1];  const hexa16 = [2,2,2,1,2,2];  const hexa31 = [2,2,1,1,1,2];  const hexa46 = [2,1,1,2,2,2];  const hexa61 = [1,1,2,2,1,1];
+    const hexa2 = [2,2,2,2,2,2];  const hexa17 = [1,2,2,1,1,2];  const hexa32 = [2,1,1,1,2,2];  const hexa47 = [2,1,2,1,1,2];  const hexa62 = [2,2,1,1,2,2];
+    const hexa3 = [1,2,2,2,1,2];  const hexa18 = [2,1,1,2,2,1];  const hexa33 = [2,2,1,1,1,1];  const hexa48 = [2,1,1,2,1,2];  const hexa63 = [1,2,1,2,1,2];
+    const hexa4 = [2,1,2,2,2,1];  const hexa19 = [1,1,2,2,2,1];  const hexa34 = [1,1,1,1,2,2];  const hexa49 = [1,2,1,1,1,2];  const hexa64 = [2,1,2,1,2,1];
+    const hexa5 = [1,1,1,2,1,2];  const hexa20 = [2,2,2,2,1,1];  const hexa35 = [2,2,2,1,2,1];  const hexa50 = [2,1,1,1,2,1];
+    const hexa6 = [2,1,2,1,1,1];  const hexa21 = [1,2,2,1,2,1];  const hexa36 = [1,2,1,2,2,2];  const hexa51 = [1,2,2,1,2,2];
+    const hexa7 = [2,1,2,2,2,2];  const hexa22 = [1,2,1,2,2,1];  const hexa37 = [1,2,1,2,1,1];  const hexa52 = [2,2,1,2,2,1];
+    const hexa8 = [2,2,2,2,1,2];  const hexa23 = [2,2,2,2,2,1];  const hexa38 = [1,1,2,1,2,1];  const hexa53 = [2,2,1,2,1,1];
+    const hexa9 = [1,1,1,2,1,1];  const hexa24 = [1,2,2,2,2,2];  const hexa39 = [2,2,1,2,1,2];  const hexa54 = [1,1,2,1,2,2];
+    const hexa10= [1,1,2,1,1,1];  const hexa25 = [1,1,1,2,2,1];  const hexa40 = [2,1,2,1,2,2];  const hexa55 = [1,2,1,1,2,2];
+    const hexa11= [1,1,1,2,2,2];  const hexa26 = [1,1,1,2,2,1];  const hexa41 = [1,1,2,2,2,1];  const hexa56 = [2,2,1,1,2,1];
+    const hexa12= [2,2,2,1,1,1];  const hexa27 = [1,2,2,2,2,1];  const hexa42 = [1,2,2,2,1,1];  const hexa57 = [2,1,1,2,1,1];
+    const hexa13= [1,2,1,1,1,1];  const hexa28 = [2,1,1,1,1,2];  const hexa43 = [1,1,1,1,1,2];  const hexa58 = [1,1,2,1,1,2];
+    const hexa14= [1,1,1,1,2,1];  const hexa29 = [2,1,2,2,1,2];  const hexa44 = [2,1,1,1,1,1];  const hexa59 = [2,1,2,2,1,1];
+    const hexa15= [2,2,1,2,2,2];  const hexa30 = [1,2,1,1,2,1];  const hexa45 = [2,2,2,1,1,2];  const hexa60 = [1,1,2,2,1,2];
+    
+
+    if(lhexa.equals(hexa1)){alert("1. Ch'ien");}      if(lhexa.equals(hexa16)){alert("16. Yü");}       if(lhexa.equals(hexa31)){alert("31. Hsien");}      if(lhexa.equals(hexa46)){alert("46. Sheng");}  if(lhexa.equals(hexa61)){alert("61. Chung Fu");}
+    if(lhexa.equals(hexa2)){alert("2. K'un");}        if(lhexa.equals(hexa17)){alert("17. Sui");}      if(lhexa.equals(hexa32)){alert("32. Heng");}       if(lhexa.equals(hexa47)){alert("47. K'un");}   if(lhexa.equals(hexa62)){alert("62. Hsiao Kuo");}
+    if(lhexa.equals(hexa3)){alert("3. Chun");}        if(lhexa.equals(hexa18)){alert("18. Ku");}       if(lhexa.equals(hexa33)){alert("33. Tun");}        if(lhexa.equals(hexa48)){alert("48. Ching");}  if(lhexa.equals(hexa63)){alert("63. Chi Chi");}
+    if(lhexa.equals(hexa4)){alert("4. Meng");}        if(lhexa.equals(hexa19)){alert("19. Lin");}      if(lhexa.equals(hexa34)){alert("34. Ta Chuang");}  if(lhexa.equals(hexa49)){alert("49. Ko");}     if(lhexa.equals(hexa64)){alert("64. Wei Chi");}
+    if(lhexa.equals(hexa5)){alert("5. Hsü");}         if(lhexa.equals(hexa20)){alert("20. Kuan");}     if(lhexa.equals(hexa35)){alert("35. Chin");}       if(lhexa.equals(hexa50)){alert("50. Ting");}
+    if(lhexa.equals(hexa6)){alert("6. Sung");}        if(lhexa.equals(hexa21)){alert("21. Shin Ho");}  if(lhexa.equals(hexa36)){alert("36. Ming I");}     if(lhexa.equals(hexa51)){alert("51. Chen");}
+    if(lhexa.equals(hexa7)){alert("7. Shih");}        if(lhexa.equals(hexa22)){alert("22. Pi");}       if(lhexa.equals(hexa37)){alert("37. Chia Jen");}   if(lhexa.equals(hexa52)){alert("52. Ken");}
+    if(lhexa.equals(hexa8)){alert("8. Pi");}          if(lhexa.equals(hexa23)){alert("23. Po");}       if(lhexa.equals(hexa38)){alert("38. K'uei");}      if(lhexa.equals(hexa53)){alert("53. Chien");}
+    if(lhexa.equals(hexa9)){alert("9. Hsiao Ch'u");}  if(lhexa.equals(hexa24)){alert("24. Fu");}       if(lhexa.equals(hexa39)){alert("39. Chien");}      if(lhexa.equals(hexa54)){alert("54. Kuei Mei");}
+    if(lhexa.equals(hexa10)){alert("10. Lü");}        if(lhexa.equals(hexa25)){alert("25. Wu Wang");}  if(lhexa.equals(hexa40)){alert("40. Hsieh");}      if(lhexa.equals(hexa55)){alert("55. Feng");}
+    if(lhexa.equals(hexa11)){alert("11. Tai");}       if(lhexa.equals(hexa26)){alert("26. Ta Ch'u");}  if(lhexa.equals(hexa41)){alert("41. Sun");}        if(lhexa.equals(hexa56)){alert("56. Lü");}
+    if(lhexa.equals(hexa12)){alert("12. P'i");}       if(lhexa.equals(hexa27)){alert("27. I");}        if(lhexa.equals(hexa42)){alert("42. I");}          if(lhexa.equals(hexa57)){alert("57. Sun");}
+    if(lhexa.equals(hexa13)){alert("13. Tung jen");}  if(lhexa.equals(hexa28)){alert("28. Ta Kuo");}   if(lhexa.equals(hexa43)){alert("43. Kuai");}       if(lhexa.equals(hexa58)){alert("58. Tui");}
+    if(lhexa.equals(hexa14)){alert("14. Ta Yu");}     if(lhexa.equals(hexa29)){alert("29. Kan");}      if(lhexa.equals(hexa44)){alert("44. Kou");}        if(lhexa.equals(hexa59)){alert("59. Huan");}
+    if(lhexa.equals(hexa15)){alert("15. Ch'ien");}    if(lhexa.equals(hexa30)){alert("30. Li");}       if(lhexa.equals(hexa45)){alert("45. Ts'ui");}      if(lhexa.equals(hexa60)){alert("60. Chieh");}
+
+  }
+
+
+  Array.prototype.equals = function (getArray) {
+  if (this.length != getArray.length) return false;
+
+  for (var i = 0; i < getArray.length; i++) {
+    if (this[i] instanceof Array && getArray[i] instanceof Array) {
+      if (!this[i].equals(getArray[i])) return false;
+    } else if (this[i] != getArray[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
